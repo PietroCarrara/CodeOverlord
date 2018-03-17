a8eee395189fb5274ce8527b843fd8e8df20c67d
Pietro Carrara <piticarrara@gmail.com>
2018-03-17 12:33:39 -0300

    Add release option for build script

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-msbuild
-cd ./CodeOverlord/bin/DesktopGL/AnyCPU/Debug/
+#!/bin/sh
+
+if [[ "$1" = "release" ]]; then
+	msbuild /t:Build /p:Configuration=Release
+	cd ./CodeOverlord/bin/DesktopGL/AnyCPU/Release/
+else
+	msbuild
+	cd ./CodeOverlord/bin/DesktopGL/AnyCPU/Debug/
+fi
+
 mono CodeOverlord.exe
 cd ../../../../../
